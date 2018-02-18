988efaf8b7a684c680b67ba543fb3108369c7895
Heitor Silva <heitor-silva@hotmail.com>
2018-02-18 15:33:29 -0300

    Add App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <div className="App-title">Music Master</div>
         <div>
           <input placeholder="search an artist" />
-          <button>button</button>
+          <button>submit</button>
         </div>
         <div className="Artist-profile">
           <div className="Artist-picture"></div>
