dc043a2d100b40c5f2204009678d18fda55cb164
Heitor Silva <heitor-silva@hotmail.com>
2018-02-18 15:37:49 -0300

    Change search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-title">Music Master</div>
-        <div>
-          <input placeholder="search an artist" />
-          <button>submit</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" placeholder="Search for an artist" />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
         <div className="Artist-profile">
           <div className="Artist-picture"></div>
           <div className="Artist-name"></div>
