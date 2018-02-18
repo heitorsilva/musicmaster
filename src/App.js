3de747ed49be2362214093d2ceb6e77338b27e52
Heitor Silva <heitor-silva@hotmail.com>
2018-02-18 15:27:50 -0300

    Add index with App component

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App-title">Music Master</div>
+        <div>
+          <input placeholder="search an artist" />
+          <button></button>
+        </div>
+        <div className="Artist-profile">
+          <div className="Artist-picture"></div>
+          <div className="Artist-name"></div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
