8c36b87b33b2116224e0ac4cde00d3e35d1fcedd
Heitor Silva <heitor-silva@hotmail.com>
2018-02-18 15:53:04 -0300

    Add search method to App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ''
+    };
+  }
+
+  search() {}
+
   render() {
     return (
       <div className="App">
         <div className="App-title">Music Master</div>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="Search for an artist" />
-            <InputGroup.Addon>
+            <FormControl
+              type="text"
+              placeholder="Search for an artist"
+              value={this.state.query}
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search() }>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
